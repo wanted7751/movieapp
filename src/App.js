f81fd1717980bb31a9253f1980604168ba0b914f
parkminho <minho4377@daum.net>
2019-01-01 21:24:59 +0900

    #7-1. Updating Movie Component

--- src/App.js
+++ src/App.js
@@ -6,17 +6,40 @@ class App extends Component {
   }
 
   componentDidMount() {
-    fetch("https://yts.am/api/v2/list_movies.json?quality=3D?sort_by=like_count")
-    .then(response => response.json())
-    .then(json => console.log(json))
-    .catch(err => console.log(err))
+    this._getMovies();
   }
 
   _renderMovies = () => {
-    const movies = this.state.movies.map((movie, key) => {
-        return <Movie title={movie.title} poster={movie.poster} key={key} />
+    const movies = this.state.movies.map((movie)  => {
+        console.log(movies)
+        return <Movie 
+        title={movie.title_english} 
+        poster={movie.medium_cover_image} 
+        key={movie.id} 
+        genres={movie.genres}
+        synopsis={movie.synopsis}
+        
+        />
       })
       return movies;
+  }
+
+  _getMovies = async() => {
+    const _movies =  await this._callApi()
+    this.setState({ movies: _movies });
+      //만약 _movies가아니고movies 면 setstate에 movies와 같으므로
+      //그냥 movies 라고 적어도됨 es6 문법
+
+  }
+
+  _callApi = ()=>{
+    return (
+      fetch("https://yts.am/api/v2/list_movies.json?quality=3D?sort_by=like_count")
+        .then(response => response.json())
+        .then(json => json.data.movies)
+        //.then(json => console.log(json))
+        .catch(err => console.log(err))
+    );
   }
 
 
--- src/Movie.js
+++ src/Movie.js
@@ -8,31 +8,58 @@ export default class Movie extends Component {
 
     static propTypes = {
         title: PropTypes.string.isRequired,
-        poster: PropTypes.string.isRequired
+        poster: PropTypes.string.isRequired,
+        genres: PropTypes.array.isRequired,
+        synopsis: PropTypes.string.isRequired
+
     }
 
   render() {
-      
-    return (
-        <div>
-            <MoviePoster poster={this.props.poster} />
-            <h1>{this.props.title}</h1>
+      console.log(this.props)
+      return(
+      <div className="Movie">
+          <div className="Movie__Columns">
+              <MoviePoster poster={this.props.poster} alt={this.props.title}/>
+          </div>
+          <div className="Movie__Columns">
+              <h1>{this.props.title}</h1>
+              <div className="Movie__Genres">
+                {this.props.genres.map((genre, index) => <MovieGenre genre={genre} key={index} />)}
+              </div>
+              <p className="Movie__Synopsis">
+                    {this.props.synopsis}
+              </p>
+        </div>
       </div>
-    )
+      )
   }
+}
+
+class MovieGenre extends Component{
+    render(){
+        return(
+            <span className="Movie__Genre">{this.props.genre}</span>
+        )
+    }
 }
 
 class MoviePoster extends Component{
 
     static propTypes = {
-        poster: PropTypes.string.isRequired
+        poster: PropTypes.string.isRequired,
+        alt: PropTypes.string.isRequired,
+
     }
 
     render(){
         
         return (
             <div>
-                <img src={this.props.poster} alt={"please"} />
+                <img src={this.props.poster} 
+                title={this.props.alt} 
+                alt={this.props.alt}
+                className="Movie__Poster"
+                />
             </div>           
         )
         
