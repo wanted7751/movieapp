fab982576d4b7f0151e4c7565e894c45c6e8ee6a
parkminho <minho4377@daum.net>
2019-01-01 10:37:00 +0900

    #6-2. Promises

--- src/App.js
+++ src/App.js
@@ -6,35 +6,10 @@ class App extends Component {
   }
 
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({
-        movies: [
-          {
-            title: "hunger",
-            poster:
-              "https://hungergamesnet.files.wordpress.com/2014/01/catchingfire-hungergames.jpg?w=869"
-          },
-          {
-            title: "oldboy",
-            poster: "https://i.ytimg.com/vi/VwIIDzrVVdc/maxresdefault.jpg"
-          },
-          {
-            title: "meet",
-            poster:
-              "https://t1.daumcdn.net/movie/8cf59a557bc7055aca6f6879f07e91dd76192d3e"
-          },
-          {
-            title: "mother",
-            poster: "http://img.newspim.com/news/2016/09/24/1609241336529940.jpg"
-          },
-          {
-            title: "soldier save",
-            poster: "http://t1.daumcdn.net/liveboard/movie/018fcad2ec2c44ae94acafa191502bbb.jpg"
-          }
-
-        ]
-      });
-    }, 2000);
+    fetch("https://yts.am/api/v2/list_movies.json?quality=3D?sort_by=like_count")
+    .then(response => response.json())
+    .then(json => console.log(json))
+    .catch(err => console.log(err))
   }
 
   _renderMovies = () => {
